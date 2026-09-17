a923a4ff74578a9634b4179112f651f654dfe84e
Mohd Aqdus <mohdaqdus809@gmail.com>
2026-09-17 21:12:26 +0530

    added Mysql connection file

---
+++ backend/config/db.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }, // TiDB requires this SSL setting
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+pool.getConnection()
+  .then((conn) => {
+    console.log("✅ TiDB Cloud Database connected successfully!");
+    conn.release();
+  })
+  .catch((err) => {
+    console.error("❌ Database Connection Failed:", err.message);
+  });
+
+module.exports = pool;
